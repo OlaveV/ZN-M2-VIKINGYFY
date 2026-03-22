fb07646a3f53e6526cb052c4d21f5a219d5d907a
OlaveV <heifu303@gmail.com>
2026-03-22 11:04:04 +0800

    Change luci-theme-argon package source
    Updated the package update command for luci-theme-argon to pull from a different repository and branch.

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -45,7 +45,8 @@ UPDATE_PACKAGE() {
 # UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf" 这样会把原有的open-app-filter，luci-app-appfilter，oaf相关组件删除，不会出现coremark错误。
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
-UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
+# 只拉取主题，不带自定义配置插件
+UPDATE_PACKAGE "luci-theme-argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "master" "pkg"
 UPDATE_PACKAGE "diskman" "lisaac/luci-app-diskman" "master"
